1a8b92132f36e756ad74ede8981d5c5561f22eb0
Marcos <marcosmp1989@hotmail.com>
2024-04-22 20:22:14 -0300

    inicio FESFSUS

---
+++ src/main/java/br/com/cairu/fesfsus/FesfsusApplication.java
@@ -0,0 +1,13 @@
+package br.com.cairu.fesfsus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FesfsusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FesfsusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cairu/fesfsus/FesfsusApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cairu.fesfsus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FesfsusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
